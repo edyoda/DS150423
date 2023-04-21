bf3869b8374dc7b9b1393781d6250356867bf3f1
Bharati Yadav <bharatiyadav14@gmail.com>
2023-04-21 21:20:37 +0530

    operator

---
+++ Python/_2_operator/_1_arithmetic_operators.py
@@ -0,0 +1,31 @@
+# + 
+# -
+# * 
+# /  - always returns a float value
+# %  - Modulus (Mod)  - returns remainder
+# // - Floor Division - convert that float and return int   3.9 -> 3
+# ** - Exponential    - power of , raise to
+
+no1 = 10
+no2 = 5
+
+add = no1 + no2
+print("Addition : ",add)
+
+sub = no1 - no2
+print("Subtraction : ",sub)
+
+multi=no1*no2
+print("Multiplication : ",multi)
+
+div=no1/no2
+print("Division : ",div)
+
+mod=no1%no2
+print("Modulus : ",mod)
+
+floordiv=no1//no2
+print("Floor Division : ",floordiv)
+
+pow=no1**no2
+print("Power : ",pow)
---
+++ Python/_2_operator/_2_comparison_operator.py
@@ -0,0 +1,29 @@
+# ==
+# !=
+# >
+# <
+# >=
+# <=
+
+# NOTE : all the above operators will return bool value
+
+no1 = 10
+no2 = 9
+
+eq = no1 == no2
+print("Equal : ",eq)
+
+noteq = no1 != no2
+print("Not equal : ",noteq)
+
+greaterthan = no1 > no2
+print("Greater than : ",greaterthan)
+
+lessthan = no1 < no2
+print("Less than : ",lessthan)
+
+greaterthaneq = no1 >= no2
+print("Greater than equal to : ",greaterthaneq)
+
+lessthaneq = no1 <= no2
+print("Less than equal to : ",lessthaneq)
