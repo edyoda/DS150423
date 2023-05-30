66a472099944d5d2dd036e47e67fa018f05342e8
Bharati Yadav <bharatiyadav14@gmail.com>
2023-05-30 19:58:22 +0530

    inheritance

---
+++ Python/_16_oops/_3_inheritance/_11_super_in_multiple_inheritance.py
@@ -0,0 +1,37 @@
+class C:
+    def __init__(self):
+        print("C = Parent Class")
+
+    def functional(self):
+        print("Functional")
+
+    def language(self):
+        print("C")
+
+class Cpp:
+    def __init__(self):
+        print("Cpp = Parent Class")
+
+    def procedural(self):
+        print("Procedural")
+
+    def language(self):
+        print("Cpp")
+
+class Python(Cpp,C):
+    def __init__(self):
+        print("Python = Child Class")
+
+    def both(self):
+        print("Functional and Procedural")
+
+    def language(self):
+        C.language(self)
+        Cpp.language(self)
+        print("Python")
+
+python = Python()
+python.functional()
+python.procedural()
+python.both()
+python.language()
